826cd590f8bf4d5ab47242a625dacd9e89d263b7
Kai Devrim <kkgithub@devrim.tech>
2020-05-24 20:41:44 -0700

    started the graduation project

---
+++ Graduation/Assets/Scripts/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public string nameSet(string name)
+    {
+        Debug.Log(name);
+        return name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
