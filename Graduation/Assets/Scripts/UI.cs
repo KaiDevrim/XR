7fe4099c43b27fd8927c9e3281204b07b79a112b
Kai Devrim <kkgithub@devrim.tech>
2020-05-25 00:11:13 -0700

    save the name into a variable

--- Graduation/Assets/Scripts/UI.cs
+++ Graduation/Assets/Scripts/UI.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class UI : MonoBehaviour
 {
+    
+    public TMP_InputField NameField;
+
     // Start is called before the first frame update
     void Start()
     {
        
     }
 
-    public string nameSet(string name)
-    {
-        Debug.Log(name);
-        return name;
-    }
-
     // Update is called once per frame
     void Update()
     {
-       
+        string name = NameField.text;
+        Debug.Log(name);
+
     }
 }
